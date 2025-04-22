b64af35173fd3106f2a722176aff50a5b82aea18
AMANULLAH023HY <amana170829@gmail.com>
2025-04-22 08:08:41 +0530

    array reverse question

---
+++ array.js
@@ -0,0 +1,8 @@
+// 1. reverse an array 
+const arr = [1,2,3,4,5,6,7,8,9];
+// console.log(arr.reverse()); // direct method use 
+let arr2 = [];
+let j = arr.length -1;
+for (let i =arr.length -1; i>=0;i--){
+    arr2.push(arr[i]) 
+}
